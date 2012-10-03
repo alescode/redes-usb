67a63fefc5eb4fce35e58280705fcf2073868ac7
Alejandro Machado <alemacgo@gmail.com>
2012-10-02 22:19:42 -0430

    Ajusta formatos de entrada al enunciado del proyecto

--- p1/ordenes.cpp
+++ p1/ordenes.cpp
@@ -10,16 +10,24 @@
 using namespace std;
 
 map<string, int> tabla_pedidos;
-// relaciona nombres de productos a la cantidad que se desea ordenar
+// relaciona nombre del producto a la cantidad que se desea ordenar
 
-int main(int argc, char** argv) {
-    if (argc != 2) {
-        cout << "Uso: ./cargar_pedidos <nombre de archivo>" << endl;
-        return 1;
-    }
+map<string, string>tabla_proveedor_direccion;
+// relaciona nombre del proveedor a direccion
+// no se encuentra <tuple> en Mac OS X, habría sido más cómodo usar
+// un solo map
 
-    ifstream datos; // archivo de entrada
-    datos.open(argv[1]);
+map<string, int>tabla_proveedor_puerto;
+
+int imprimir_uso() {
+    cerr << "Uso: ordenes -[a|b] -f [archivo de pedidos] -d "
+        << "[archivo de proveedores]" << endl;
+    return 1; // reportar error a la consola
+}
+
+void inicializar_tabla_pedidos(string archivo_pedidos) {
+    ifstream datos;
+    datos.open(archivo_pedidos.c_str());
 
     if (datos.is_open()) {
         string linea;
@@ -39,12 +47,103 @@ int main(int argc, char** argv) {
         }
     }
     else {
-        cerr << "ERROR: No se pudo abrir el archivo." << endl;
-        return 1;
+        cerr << "ERROR: No se pudo abrir el archivo de pedidos." << endl;
+        exit(1);
     }
     datos.close();
+}
 
-    print_map(tabla_pedidos);
+void imprimir_tabla_pedidos() {
+    map<string, int>::const_iterator pos;
+    cout << "{";
+    for (pos = tabla_pedidos.begin(); pos != tabla_pedidos.end(); ++pos) {
+        cout << "\"" << pos->first << "\": ";
+        cout << pos->second << ", ";
+    }
+    cout << "}" << endl;
+}
+
+void imprimir_tabla_proveedores() {
+    map<string, string>::const_iterator pos;
+    cout << "{";
+    for (pos = tabla_proveedor_direccion.begin(); 
+         pos != tabla_proveedor_direccion.end(); ++pos) {
+        cout << "\"" << pos->first << "\": <";
+        cout << pos->second << ", ";
+        cout << tabla_proveedor_puerto[pos->first] << ">, ";
+    }
+    cout << "}" << endl;
+}
+
+void inicializar_tablas_proveedor(string archivo_proveedores) {
+    ifstream datos;
+    datos.open(archivo_proveedores.c_str());
+
+    if (datos.is_open()) {
+        string linea;
+        string resto;
+        string nombre_proveedor;
+        string direccion_proveedor;
+        int puerto_proveedor;
+        while (datos.good()) {
+            getline(datos, linea);
+            if (linea != "") {
+
+                // se llena la tabla de pedidos
+                int pos_separador = linea.find("&");
+                nombre_proveedor = trim(linea.substr(0, pos_separador));
+                resto = linea.substr(pos_separador + 1, linea.length());
+
+                int pos_separador_2 = resto.find("&");
+                direccion_proveedor = trim(resto.substr(0, pos_separador_2));
+
+                istringstream s(linea.substr(pos_separador_2 + 1, linea.length()));
+                s >> puerto_proveedor;
+
+                tabla_proveedor_direccion[nombre_proveedor] = direccion_proveedor;
+                tabla_proveedor_puerto[nombre_proveedor] = puerto_proveedor;
+            }
+        }
+    }
+    else {
+        cerr << "ERROR: No se pudo abrir el archivo de proveedores." << endl;
+        exit(1);
+    }
+    datos.close();
+}
+
+int basico(string archivo_pedidos, string archivo_proveedores) {
+    inicializar_tabla_pedidos(archivo_pedidos);
+    inicializar_tablas_proveedor(archivo_proveedores);
+
+    imprimir_tabla_pedidos();
+    imprimir_tabla_proveedores();
+    //print_map_2(tabla_proveedores);
 
     return 0;
+}
+
+int avanzado(string archivo_pedidos, string archivo_proveedores) {
+    cout << "no implementado" << endl;
+    return 1;
+}
+
+int main(int argc, char** argv) {
+    if (argc != 6) {
+        return imprimir_uso();
+    }
+
+    string uso = string(argv[1]);
+    string archivo_pedidos = string(argv[3]);
+    string archivo_proveedores = string(argv[5]);
+
+    if (uso == "-a") {
+        return avanzado(archivo_pedidos, archivo_proveedores);
+    }
+    else if (uso == "-b") {
+        return basico(archivo_pedidos, archivo_proveedores);
+    }
+    else {
+        return imprimir_uso();
+    }
 }
